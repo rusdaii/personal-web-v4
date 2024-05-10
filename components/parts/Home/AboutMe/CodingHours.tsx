f8fdcf288a134f5b3e39193c940f7ea275241c2d
rusdaii <rusdaii.html@gmail.com>
2024-05-10 07:07:04 +0700

    refactor:caching wakatime api & modify codinghours component

--- components/parts/Home/AboutMe/CodingHours.tsx
+++ components/parts/Home/AboutMe/CodingHours.tsx
@@ -10,12 +10,8 @@ const CodingHours = () => {
   const { data: wakatimeData } = useSWR<Wakatime>('/api/wakatime', fetcher);
 
   const capturedTime = wakatimeData?.seconds
-    ? Math.round(wakatimeData.seconds / 60 / 60)
-    : 0;
-
-  const nonCapturedTime = 2348;
-
-  const totalTime = capturedTime + nonCapturedTime;
+    ? Math.round(wakatimeData.seconds / 60 / 60) + 1800 // time since using wakatime + time spent before using wakatime
+    : '--';
 
   return (
     <div className="flex flex-col gap-6 p-4 rounded-xl shadow-feature-card dark:shadow-feature-card-dark lg:p-6">
@@ -24,7 +20,7 @@ const CodingHours = () => {
         <h2 className="text-sm font-light">Coding hours</h2>
       </div>
       <div className="flex items-center justify-center text-4xl font-semibold grow font-title">
-        {totalTime} hrs
+        {capturedTime} hrs
       </div>
     </div>
   );
