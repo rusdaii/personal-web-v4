25752709d51c5c787a8a4eae0c0400d5393a250b
rusdaii <rusdaii.html@gmail.com>
2024-05-11 20:23:50 +0700

    feat:change runtime to bun

--- app/api/disable-draft/route.ts
+++ app/api/disable-draft/route.ts
@@ -3,6 +3,6 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export function GET(request: NextRequest) {
   draftMode().disable();
-  const url = new URL(request.nextUrl);
+  const url = new URL(request.nextUrl.toString());
   return NextResponse.redirect(new URL('/', url.origin));
 }
